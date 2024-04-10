eaf51cf0652c24d24cdb4fbde76a8b34e096b9fc
shubham4635 <shubhamkangune4635@gmail.com>
2024-04-10 15:59:31 +0530

    add new changes

--- src/Basic.java
+++ src/Basic.java
@@ -9,6 +9,7 @@ public class Basic {
         System.setProperty("webdriver.chrome.driver", "D:\\selenium files\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();driver.get("http://toolsqa.com/automation-practice-table/");
         List<WebElement>tData=driver.findElements(By.tagName("td"));
+        System.out.println("hello asjkk");
         for (WebElement data :tData){
             System.out.println(data.getText());
         }
